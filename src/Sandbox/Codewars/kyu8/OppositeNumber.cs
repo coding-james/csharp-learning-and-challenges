50ffbb9b417dd96264c6e7533e073b832c48115b
James <98489957+coding-james@users.noreply.github.com>
2023-10-12 20:45:57 +0100

    Codewars 8 kyu challenges

---
+++ src/Sandbox/Codewars/kyu8/OppositeNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Codewars.kyu8
+{
+    public class OppositeNumber
+    {
+        // CODEWARS: Opposite Number
+        public static int Opposite(int number)
+        {
+            return number - (number * 2);
+        }
+    }
+}
---
+++ test/Sandbox.Tests/CodewarsTests/kyu8/OppositeNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Tests.CodewarsTests.kyu8
+{
+    public class OppositeNumber
+    {
+        // CODEWARS: Opposite Number
+        [Theory]
+        [InlineData(-1, 1)]
+        [InlineData(-14, 14)]
+        [InlineData(34, -34)]
+
+        public void Opposite(int expected, int input)
+        {
+            Assert.Equal(expected, Codewars.kyu8.OppositeNumber.Opposite(input));
+        }
+    }
+}
