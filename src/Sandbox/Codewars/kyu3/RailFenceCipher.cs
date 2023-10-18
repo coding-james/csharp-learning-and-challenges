00467918e602bc9c73544d5a9f76f89f40d51b0f
James <98489957+coding-james@users.noreply.github.com>
2023-10-18 13:01:26 +0100

    Codewars 3 kyu challenges

---
+++ src/Sandbox/Codewars/kyu3/RailFenceCipher.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sandbox.Codewars.kyu3
+{
+    public class RailFenceCipher
+    {
+        // CODEWARS: Rail Fence Cipher: Encoding and Decoding
+        public static string Encode(string s, int n)
+        {
+            if (string.IsNullOrEmpty(s)) { return ""; }
+
+            List<string> segments = new List<string>();
+            string direction = "backwards";
+            string result = "";
+            int pos = n - 2;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (segments.Count < n)
+                {
+                    segments.Add(s.Substring(i, 1));
+                }
+                else if (direction == "forwards")
+                {
+                    segments[pos] += s.Substring(i, 1);
+                    if (pos < (n) - 1)
+                    {
+                        pos++;
+                    }
+                    else
+                    {
+                        pos--;
+                        direction = "backwards";
+                    }
+                }
+                else
+                {
+                    segments[pos] += s.Substring(i, 1);
+                    if (pos > 0)
+                    {
+                        pos--;
+                    }
+                    else
+                    {
+                        pos++;
+                        direction = "forwards";
+                    }
+                }
+            }
+
+            for (int j = 0; j < segments.Count; j++)
+            {
+                result += segments[j];
+            }
+
+            return result;
+        }
+
+        public static string Decode(string s, int n)
+        {
+            if (string.IsNullOrEmpty(s)) { return ""; }
+            List<string> segments = new List<string>();
+            List<int> code = new List<int>();
+            string direction = "backwards";
+            string result = "";
+            int pos = n - 2;
+
+            // Calculate rails
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (code.Count < n)
+                {
+                    code.Add(i);
+                }
+                else if (direction == "forwards")
+                {
+                    code.Add(pos);
+                    if (pos < (n) - 1)
+                    {
+                        pos++;
+                    }
+                    else
+                    {
+                        pos--;
+                        direction = "backwards";
+                    }
+                }
+                else
+                {
+                    code.Add(pos);
+                    if (pos > 0)
+                    {
+                        pos--;
+                    }
+                    else
+                    {
+                        pos++;
+                        direction = "forwards";
+                    }
+                }
+            }
+            code.Sort();
+
+            // Split into segments
+            int segmentIndex = -1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == 0 || code[i] != code[i - 1])
+                {
+                    segments.Add(s.Substring(i, 1));
+                    segmentIndex++;
+                }
+                else
+                {
+                    segments[segmentIndex] += s.Substring(i, 1);
+                }
+            }
+
+            // Take chars from segments
+            segmentIndex = 0;
+            direction = "forwards";
+            while (result.Length < s.Length)
+            {
+                if (direction == "forwards")
+                {
+                    result += segments[segmentIndex][0];
+                    segments[segmentIndex] = segments[segmentIndex].Substring(1);
+                    if (segmentIndex < segments.Count - 1)
+                    {
+                        segmentIndex++;
+                    }
+                    else
+                    {
+                        segmentIndex--;
+                        direction = "backwards";
+                    }
+                }
+                else
+                {
+                    result += segments[segmentIndex][0];
+                    segments[segmentIndex] = segments[segmentIndex].Substring(1);
+                    if (segmentIndex > 0)
+                    {
+                        segmentIndex--;
+                    }
+                    else
+                    {
+                        segmentIndex++;
+                        direction = "forwards";
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ test/Sandbox.Tests/CodewarsTests/kyu3/RailFenceCipher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Tests.CodewarsTests.kyu3
+{
+    public class RailFenceCipher
+    {
+        // CODEWARS: Rail Fence Cipher: Encoding and Decoding
+        [Theory]
+        [InlineData("WECRLTEERDSOEEFEAOCAIVDEN", "WEAREDISCOVEREDFLEEATONCE", 3)]
+        [InlineData("Hoo!el,Wrdl l", "Hello, World!", 3)]
+        [InlineData("H !e,Wdloollr", "Hello, World!", 4)]
+        [InlineData("", "", 3)]
+        [InlineData("ABC", "ABC", 3)]
+        [InlineData("AB", "AB", 2)]
+        [InlineData("Tacotvyohtakhua eea dt b slhyi wIa t t dwolt  yyhtou' uehed aei ovd bahoidy yyeeg tt s 'wo  sot  a erou,sit'asbgher woltnnlowot hAomaaoyeuon gtnnee dbetnneo d hrd of  abna yo oId yu nbw yo", "Today is gonna be the day that they're gonna throw it back to you And by now, you should've somehow realised what you gotta do I don't believe that anybody feels the way I do about you now", 15)]
+        [InlineData("T'yieaoyrbnlsivobdeedoaeledoahgnwedetIuytoA,rwbhytitnuywhtaaysanoooa'twoghayuhtnaeuottoseyonhnnyhthmodytonarkoouIbswadocusgoolbewaleoddeehibdvtayett'tf", "Todayisgonnabethedaythatthey'regonnathrowitbacktoyouAndbynow,youshould'vesomehowrealisedwhatyougottadoIdon'tbelievethatanybodyfeelsthewayIdoaboutyounow", 15)]
+
+        public void RailFenceCipher_Encode(string expected, string input1, int input2)
+        {
+            Assert.Equal(expected, Codewars.kyu3.RailFenceCipher.Encode(input1, input2));
+        }
+
+        [Theory]
+        [InlineData("WEAREDISCOVEREDFLEEATONCE", "WECRLTEERDSOEEFEAOCAIVDEN", 3)]
+        [InlineData("Hello, World!", "Hoo!el,Wrdl l", 3)]
+        [InlineData("Hello, World!", "H !e,Wdloollr", 4)]
+        [InlineData("", "", 3)]
+        [InlineData("ABC", "ABC", 3)]
+        [InlineData("AB", "AB", 2)]
+        [InlineData("Today is gonna be the day that they're gonna throw it back to you And by now, you should've somehow realised what you gotta do I don't believe that anybody feels the way I do about you now", "Tacotvyohtakhua eea dt b slhyi wIa t t dwolt  yyhtou' uehed aei ovd bahoidy yyeeg tt s 'wo  sot  a erou,sit'asbgher woltnnlowot hAomaaoyeuon gtnnee dbetnneo d hrd of  abna yo oId yu nbw yo", 15)]
+        [InlineData("Todayisgonnabethedaythatthey'regonnathrowitbacktoyouAndbynow,youshould'vesomehowrealisedwhatyougottadoIdon'tbelievethatanybodyfeelsthewayIdoaboutyounow", "T'yieaoyrbnlsivobdeedoaeledoahgnwedetIuytoA,rwbhytitnuywhtaaysanoooa'twoghayuhtnaeuottoseyonhnnyhthmodytonarkoouIbswadocusgoolbewaleoddeehibdvtayett'tf", 15)]
+
+        public void RailFenceCipher_Decode(string expected, string input1, int input2)
+        {
+            Assert.Equal(expected, Codewars.kyu3.RailFenceCipher.Decode(input1, input2));
+        }
+    }
+}
