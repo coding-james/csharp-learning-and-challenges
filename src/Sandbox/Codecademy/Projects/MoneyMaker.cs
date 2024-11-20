7a335a175bd7a55f788f43751f82aaef97f2f9e9
James <98489957+coding-james@users.noreply.github.com>
2024-11-20 13:31:22 +0000

    MoneyMaker C# Numbers and Operators project

---
+++ src/Sandbox/Codecademy/Projects/MoneyMaker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Codecademy.Projects
+{
+    public class MoneyMaker
+    {
+        public static void CoinConverter(string[] args)
+        {
+            int goldCoinValue = 10;
+            int silverCoinValue = 5;
+
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Please enter the amount of coins you'd like to convert");
+            string inputAmountString = Console.ReadLine();
+            Console.WriteLine($"You entered {inputAmountString}");
+
+            double inputAmountDouble = Convert.ToDouble(inputAmountString);
+            Console.WriteLine($"{inputAmountDouble} is equal to...");
+
+            double goldCoins = Math.Floor(inputAmountDouble / goldCoinValue);
+            double remainder = inputAmountDouble % goldCoinValue;
+
+            double silverCoins = Math.Floor(remainder / silverCoinValue);
+            remainder = remainder % silverCoinValue;
+
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins: {Math.Floor(remainder)}");
+        }
+    }
+}
