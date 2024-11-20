1509416b89606b32eb01f86fbf54edd066fc64a0
James <98489957+coding-james@users.noreply.github.com>
2024-11-20 13:31:49 +0000

    SpaceExpedition bool exercise

---
+++ src/Sandbox/Codecademy/Projects/SpaceExpedition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Codecademy.Projects
+{
+    public class SpaceExpedition
+    {
+        public static void Main(string[] args)
+        {
+            // Your code goes here
+            bool isAtmosphereBreathable = true;
+            bool isGravityStable = false;
+            bool resourcesSufficient = true;
+
+            bool isHabitable = isAtmosphereBreathable && isGravityStable;
+            Console.WriteLine(isHabitable);
+
+            bool suitableForExpansion = isGravityStable || resourcesSufficient;
+            Console.WriteLine(suitableForExpansion);
+
+            int currentCrew = 5;
+            int maxCrewCapacity = 10;
+
+            bool canAcceptMoreCrew = currentCrew < maxCrewCapacity;
+            Console.WriteLine(canAcceptMoreCrew);
+        }
+    }
+}
