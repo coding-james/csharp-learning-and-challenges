ef33df12c1aac15c409e997fda8da70b043a1017
James <98489957+coding-james@users.noreply.github.com>
2024-12-02 19:02:30 +0000

    Day 1 data moved to PuzzleData and Day 2 puzzles

---
+++ src/Sandbox/AdventOfCode2024/Day02RedNosedReports.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.AdventOfCode2024
+{
+    public class Day02RedNosedReports
+    {
+        public static int HowManySafeReports(List<List<int>> reports)
+        {
+            int countOfSafe = 0;
+            int countOfUnsafe = 0;
+
+            for (int report = 0; report < reports.Count; report++)
+            {
+                if (IsItSafe(reports[report]))
+                {
+                    countOfSafe++;
+                }
+                else
+                {
+                    countOfUnsafe++;
+                }
+            }
+
+            return countOfSafe;
+        }
+
+        public static int HowManySafeReportsProblemDampener(List<List<int>> reports)
+        {
+            int countOfSafe = 0;
+            int countOfUnsafe = 0;
+
+            for (int report = 0; report < reports.Count; report++)
+            {
+                if (IsItSafe(reports[report]))
+                {
+                    countOfSafe++;
+                }
+                else
+                {
+                    int reportCount = reports[report].Count;
+                    List<bool> outcome = new List<bool>();
+
+                    for (int index = 0; index < reportCount; index++)
+                    {
+                        List<int> problemDampener = new List<int>();
+
+                        for (int i = 0; i < reports[report].Count; i++)
+                        {
+                            problemDampener.Add(reports[report][i]);
+                        }
+                        problemDampener.RemoveAt(index);
+
+                        outcome.Add(IsItSafe(problemDampener));
+                    }
+
+                    if (outcome.Where(x => x.Equals(true)).Count() >= 1)
+                    {
+                        countOfSafe++;
+                    }
+                    else
+                    {
+                        countOfUnsafe++;
+                    }
+                }
+            }
+
+            return countOfSafe;
+        }
+
+        private static bool IsItSafe(List<int> report)
+        {
+            int safetyLimitMax = 3;
+            int safetyLimitMin = 1;
+            int increasing = 0;
+            int decreasing = 0;
+            int levelsToCheck = report.Count - 1;
+            int levelUnsafe = 0;
+
+            for (int level = 0; level < levelsToCheck; level++)
+            {
+                int num1 = report[level];
+                int num2 = report[level + 1];
+                int diff = Math.Max(num1, num2) - Math.Min(num1, num2);
+
+                if ((Math.Min(increasing, decreasing) != 0 || diff == 0))
+                {
+                    levelUnsafe++;
+                    break;
+                }
+                else if (num1 > num2 && diff <= safetyLimitMax && diff >= safetyLimitMin)
+                {
+                    decreasing++;
+                }
+                else if (num1 < num2 && diff <= safetyLimitMax && diff >= safetyLimitMin)
+                {
+                    increasing++;
+                }
+                else
+                {
+                    levelUnsafe++;
+                    break;
+                }
+            }
+
+            return Math.Min(increasing, decreasing) == 0 && Math.Max(increasing, decreasing) > 0 && levelUnsafe == 0;
+        }
+    }
+}
---
+++ test/Sandbox.Tests/AdventOfCode2024Tests/Day02RedNosedReportsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.AdventOfCode2024;
+using Sandbox.Tests.AdventOfCode2024Tests.Data;
+
+namespace Sandbox.Tests.AdventOfCode2024Tests
+{
+    public class Day02RedNosedReportsTest
+    {
+        List<List<int>> exampleReports = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1], [1, 3, 2, 4, 5], [8, 6, 4, 4, 1], [1, 3, 6, 7, 9]];
+        List<List<int>> puzzleReports = PuzzleData.DayTwoList;
+
+        [Fact]
+        public void ExampleData_Safe_Or_Unsafe()
+        {
+            Assert.Equal(2, Day02RedNosedReports.HowManySafeReports(exampleReports));
+        }
+
+        [Fact]
+        public void PuzzleData_Safe_Or_Unsafe()
+        {
+            Assert.Equal(564, Day02RedNosedReports.HowManySafeReports(puzzleReports));
+        }
+
+        [Fact]
+        public void ExampleData_Safe_Or_Unsafe_Problem_Dampener()
+        {
+            Assert.Equal(4, Day02RedNosedReports.HowManySafeReportsProblemDampener(exampleReports));
+        }
+
+        [Fact]
+        public void PuzzleData_Safe_Or_Unsafe_Problem_Dampener()
+        {
+            Assert.Equal(604, Day02RedNosedReports.HowManySafeReportsProblemDampener(puzzleReports));
+        }
+    }
+}
