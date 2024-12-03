c6b989c51d92e096719d11ecb1f73b0ad43b6a9b
James <98489957+coding-james@users.noreply.github.com>
2024-12-03 20:29:43 +0000

    Day 3 puzzles

---
+++ src/Sandbox/AdventOfCode2024/Day03MullItOver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sandbox.AdventOfCode2024
+{
+    public class Day03MullItOver
+    {
+        public static int Calc(string input, bool conditionsEnabled = false)
+        {
+            int result = 0;
+            bool selector = true; 
+
+            List<string> matches = FindPatternMatches(input, conditionsEnabled);
+
+            foreach (string match in matches)
+            {
+                if (match == "do()" && conditionsEnabled)
+                {
+                    selector = true;
+                }
+                else if (match == "don't()" && conditionsEnabled)
+                {
+                    selector = false;
+                }
+                else if (selector)
+                {
+                    string[] numbers = match.Split(",");
+                    result += int.Parse(Regex.Replace(numbers[0], @"[^0-9]", "")) * int.Parse(Regex.Replace(numbers[1], @"[^0-9]", ""));
+                }
+                else
+                {
+                    continue;
+                }
+                
+            }
+
+            return result;
+        }
+
+        public static List<string> FindPatternMatches(string input, bool conditionsEnabled)
+        {
+            List<string> matchList = new List<string>();
+            MatchCollection matches;
+
+            if (conditionsEnabled)
+            {
+                matches = Regex.Matches(input, @"mul\(\d+,\d+\)|do\(\)|don't\(\)");
+            }
+            else
+            {
+                matches = Regex.Matches(input, @"mul\(\d+,\d+\)");
+            }         
+
+            foreach (Match match in matches)
+            {
+                matchList.Add(match.Value);
+            }
+            return matchList;
+        }
+    }
+}
---
+++ test/Sandbox.Tests/AdventOfCode2024Tests/Day03MullItOverTest.cs
@@ -0,0 +1,54 @@
+﻿using Sandbox.AdventOfCode2024;
+using Sandbox.Tests.AdventOfCode2024Tests.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Tests.AdventOfCode2024Tests
+{
+    public class Day03MullItOverTest
+    {
+        string examplePart1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        string examplePart2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+        [Fact]
+        public void ExamplePartOne_Finds_Correct_Patterns()
+        {
+            List<string> expectedPatterns = ["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"];
+            Assert.Equal(expectedPatterns, Day03MullItOver.FindPatternMatches(examplePart1, false));
+        }
+
+        [Fact]
+        public void ExamplePartOne()
+        {
+            Assert.Equal(161, Day03MullItOver.Calc(examplePart1));
+        }
+
+        [Fact]
+        public void PuzzlePartOne()
+        {
+            Assert.Equal(178794710, Day03MullItOver.Calc(PuzzleData.DayThree));
+        }
+
+        [Fact]
+        public void ExamplePartTwo_Finds_Correct_Patterns()
+        {
+            List<string> expectedPatterns = ["mul(2,4)", "don't()","mul(5,5)", "mul(11,8)", "do()", "mul(8,5)"];
+            Assert.Equal(expectedPatterns, Day03MullItOver.FindPatternMatches(examplePart2, true));
+        }
+
+        [Fact]
+        public void ExamplePartTwo()
+        {
+            Assert.Equal(48, Day03MullItOver.Calc(examplePart2, true));
+        }
+
+        [Fact]
+        public void PuzzlePartTwo()
+        {
+            Assert.Equal(76729637, Day03MullItOver.Calc(PuzzleData.DayThree, true));
+        }
+    }
+}
