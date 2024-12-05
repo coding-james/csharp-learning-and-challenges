00e89f1579101eebb4b22731859b1972ea854c3b
James <98489957+coding-james@users.noreply.github.com>
2024-12-05 21:45:48 +0000

    Day 4 Puzzles

---
+++ src/Sandbox/AdventOfCode2024/Day04CeresSearch.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sandbox.AdventOfCode2024
+{
+    public class Day04CeresSearch
+    {
+
+        public static int FindXmas(string input)
+        {
+            int countOfPattern = 0;
+            List<List<string>> searchPatterns = [["X", "M", "A", "S"], ["S", "A", "M", "X"]];
+            
+            List<List<string>> searchList = Breakdown(Cleanse(input));
+
+            int rowsToSearch = searchList.Count;
+            int columnsToSearch = searchList[0].Count;
+            int searchPatternLength = searchPatterns[0].Count;
+            int rowsToSearchLimited = (rowsToSearch - searchPatternLength);
+            int columnsToSearchLimited = columnsToSearch - searchPatternLength;
+
+            for (int pattern = 0; pattern < searchPatterns.Count; pattern++)
+            {
+                // Horizontal
+                for (int row = 0; row < rowsToSearch; row++)
+                {
+                    for (int col = 0; col <= columnsToSearchLimited; col++)
+                    {
+                        if (Search(row, col, row, col + 1, row, col + 2, row, col + 3, searchList, searchPatterns[pattern]))
+                        {
+                            countOfPattern++;
+                        }
+                    }
+                }
+
+                // Vertical    
+                for (int col = 0; col < columnsToSearch; col++)
+                {
+
+                    for (int row = 0; row <= rowsToSearchLimited; row++)
+                    {
+                        if (Search(row, col, row + 1, col, row + 2, col, row + 3, col, searchList, searchPatterns[pattern]))
+                        {
+                            countOfPattern++;
+                        }
+                    }
+                }
+
+                for (int col = 0; col <= columnsToSearchLimited; col++)
+                {
+
+                    for (int row = 0; row <= rowsToSearchLimited; row++)
+                    {
+                        // Diagonal Down L to R
+                        if (Search(row, col, row + 1, col + 1, row + 2, col + 2, row + 3, col + 3, searchList, searchPatterns[pattern]))
+                        {
+                            countOfPattern++;
+                        }
+
+                        // Diagonal Up L to R
+                        if (Search(row + 3, col, row + 2, col + 1, row + 1, col + 2, row + 0, col + 3, searchList, searchPatterns[pattern]))
+                        {
+                            countOfPattern++;
+                        }
+                    }
+                }
+            }
+
+                
+            return countOfPattern;
+        }
+
+        public static int FindMasX(string input)
+        {
+            int countOfPattern = 0;
+            List<List<string>> searchPatterns = [["M", "M", "A", "S", "S"], ["M", "S", "A", "M", "S"], ["S", "S", "A", "M", "M"], ["S", "M", "A", "S", "M"]];
+
+            List<List<string>> searchList = Breakdown(CleanseX(input));
+
+            int rowsToSearch = searchList.Count - 2;
+            int columnsToSearch = searchList[0].Count - 2;
+
+            for (int pattern = 0; pattern < searchPatterns.Count; pattern++)
+            {
+                for (int col = 0; col < columnsToSearch; col++)
+                {
+                    for (int row = 0; row < rowsToSearch; row++)
+                    {
+                        if (SearchX(row, col, row, col + 2, row + 1, col + 1, row + 2, col, row + 2, col + 2, searchList, searchPatterns[pattern]))
+                        {
+                            countOfPattern++;
+                        }
+                    }
+                }
+            }
+
+            return countOfPattern;
+        }
+
+        //Replace all the letters not in XMAS and break into words
+        public static List<string> Cleanse(string input)
+        {
+            List<string> cleansedList = input.Split(Environment.NewLine).ToList();
+
+            for (int index = 0; index < cleansedList.Count; index++)
+            {
+                cleansedList[index] = Regex.Replace(cleansedList[index], @"[^XMAS]", ".");
+            }
+
+            return cleansedList;
+        }
+
+        //Replace all the letters not in MAS and break into words
+        public static List<string> CleanseX(string input)
+        {
+            List<string> cleansedList = input.Split(Environment.NewLine).ToList();
+
+            for (int index = 0; index < cleansedList.Count; index++)
+            {
+                cleansedList[index] = Regex.Replace(cleansedList[index], @"[^MAS]", ".");
+            }
+
+            return cleansedList;
+        }
+
+        //Breakdown into individual characters
+        public static List<List<String>> Breakdown(List<string> input)
+        {
+
+            List<List<string>> list = new List<List<string>>();
+
+            for (int index = 0; index < input.Count; index++)
+            {
+                string sub = input[index];
+                List<string> sublist = new List<string>();
+                for (int subIndex = 0; subIndex < sub.Length; subIndex++)
+                {
+                    sublist.Add(sub.Substring(subIndex, 1));
+                }
+                list.Add(sublist);
+            }
+
+            return list;
+        }
+
+        // Search
+        public static bool Search(int row0, int col0, int row1, int col1, int row2, int col2, int row3, int col3, List<List<string>> searchList, List<string> searchPattern)
+        {
+
+            return searchList[row0][col0] == searchPattern[0]
+                   && searchList[row1][col1] == searchPattern[1]
+                   && searchList[row2][col2] == searchPattern[2]
+                   && searchList[row3][col3] == searchPattern[3];
+        }
+
+        // Search X Pattern
+        public static bool SearchX(int topLeftRow, int topLeftCol, int topRightRow, int topRightCol, int centerRow, int centerCol, int bottomLeftRow, int bottomLeftCol, int bottomRightRow, int bottomRightCol, List<List<string>> searchList, List<string> searchPattern)
+        {
+            return searchList[topLeftRow][topLeftCol] == searchPattern[0]
+                && searchList[topRightRow][topRightCol] == searchPattern[1]
+                && searchList[centerRow][centerCol] == searchPattern[2]
+                && searchList[bottomLeftRow][bottomLeftCol] == searchPattern[3]
+                && searchList[bottomRightRow][bottomRightCol] == searchPattern[4];
+
+        }
+    }
+}
---
+++ test/Sandbox.Tests/AdventOfCode2024Tests/Day04CeresSearchTest.cs
@@ -0,0 +1,92 @@
+﻿using Sandbox.AdventOfCode2024;
+using Sandbox.Tests.AdventOfCode2024Tests.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Tests.AdventOfCode2024Tests
+{
+    public class Day04CeresSearchTest
+    {
+        string exampleOne = "..X...\r\n.SAMX.\r\n.A..A.\r\nXMAS.S\r\n.X....";
+        string exampleTwo = "MMMSXXMASM\r\nMSAMXMSMSA\r\nAMXSXMAAMM\r\nMSAMASMSMX\r\nXMASAMXAMM\r\nXXAMMXXAMA\r\nSMSMSASXSS\r\nSAXAMASAAA\r\nMAMMMXMMMM\r\nMXMXAXMASX";
+        string exampleThree = "CcXxma\r\n.SAMX.\r\n.A..A.\r\nXMAS.S\r\n.X....";
+
+        List<string> expectedCleanseOneAndThree = ["..X...", ".SAMX.", ".A..A.", "XMAS.S", ".X...."];
+        List<string> expectedCleanseTwo = ["MMMSXXMASM", "MSAMXMSMSA", "AMXSXMAAMM", "MSAMASMSMX", "XMASAMXAMM", "XXAMMXXAMA", "SMSMSASXSS", "SAXAMASAAA", "MAMMMXMMMM", "MXMXAXMASX"];
+
+        List<List<string>> expectedBreakdownThree = [
+            [".",".","X",".",".","."],
+            [".","S","A","M","X","."], 
+            [".","A",".",".","A","."],
+            ["X","M","A","S",".","S"], 
+            [".","X",".",".",".","."]];
+
+        [Fact]
+        public void Cleanse_Test_Example_One()
+        {
+            Assert.Equal(expectedCleanseOneAndThree, Day04CeresSearch.Cleanse(exampleOne));
+        }
+
+        [Fact]
+        public void Cleanse_Test_Example_Two()
+        {
+            Assert.Equal(expectedCleanseTwo, Day04CeresSearch.Cleanse(exampleTwo));
+        }
+
+        [Fact]
+        public void Cleanse_Test_Example_Three()
+        {
+            Assert.Equal(expectedCleanseOneAndThree, Day04CeresSearch.Cleanse(exampleThree));
+        }
+
+        [Fact]
+        public void Breakdown_Test_Expected_Three()
+        {
+            Assert.Equal(expectedBreakdownThree, Day04CeresSearch.Breakdown(expectedCleanseOneAndThree));
+        }
+
+        [Theory]
+        [InlineData("------\r\n------\r\n------\r\n------\r\n------", 0)] // None
+        [InlineData("Xabcde\r\nMabcde\r\nAabcde\r\nSabcde\r\nabcdef", 1)] // Single Column
+        [InlineData("------\r\n-----S\r\n-----A\r\n-----M\r\n-----X", 1)] // Single Column Reverse
+        [InlineData("XMASS_\r\nabcdef\r\nabcdef\r\nabcdef\r\nabcdef", 1)] // Single Row
+        [InlineData("------\r\n------\r\n------\r\n------\r\n--SAMX", 1)] // Single Row Reverse
+        [InlineData("X----S\r\n-M----\r\n--A---\r\n---S--\r\n------", 1)] // Single L to R Down
+        [InlineData("------\r\n--S---\r\n---A--\r\n----M-\r\n-----X", 1)] // Single L to R Down Reverse
+        [InlineData("------\r\n---S--\r\n--A---\r\n-M----\r\nX-----", 1)] // Single L to R Up
+        [InlineData("------\r\n-----X\r\n----M-\r\n---A--\r\n--S---", 1)] // Single L to R Up Reverse
+        [InlineData("CcXxma\r\n.SAMX.\r\n.A..A.\r\nXMAS.S\r\n.X....", 4)]
+        [InlineData("MMMSXXMASM\r\nMSAMXMSMSA\r\nAMXSXMAAMM\r\nMSAMASMSMX\r\nXMASAMXAMM\r\nXXAMMXXAMA\r\nSMSMSASXSS\r\nSAXAMASAAA\r\nMAMMMXMMMM\r\nMXMXAXMASX", 18)]
+        public void FindXmas_Test(string input, int expected)
+        {
+            Assert.Equal(expected, Day04CeresSearch.FindXmas(input));
+        }
+
+        [Fact]
+        public void FindXmas_Puzzle_Test()
+        {
+            Assert.Equal(2718, Day04CeresSearch.FindXmas(PuzzleData.DayFour));
+        }
+
+        [Theory]
+        [InlineData("..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........", 0)]
+        [InlineData("M.M.......\r\n.A........\r\nS.S.......\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........", 1)] // ["M", "M", "A", "S", "S"] 
+        [InlineData("M.S.......\r\n.A........\r\nM.S.......\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........", 1)] // ["M", "S", "A", "M", "S"] // not working
+        [InlineData("..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n.......S.S\r\n........A.\r\n.......M.M", 1)] // ["S", "S", "A", "M", "M"]
+        [InlineData("..........\r\n.S.M......\r\n..A.......\r\n.S.M......\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........\r\n..........", 1)] // ["S", "M", "A", "S", "M"] // not working
+        [InlineData(".M.S......\r\n..A..MSMS.\r\n.M.S.MAA..\r\n..A.ASMSM.\r\n.M.S.M....\r\n..........\r\nS.S.S.S.S.\r\n.A.A.A.A..\r\nM.M.M.M.M.\r\n..........", 9)]
+        public void FindMasX_Test(string input, int expected)
+        {
+            Assert.Equal(expected, Day04CeresSearch.FindMasX(input));
+        }
+
+        [Fact]
+        public void FindMasX_Puzzle_Test()
+        {
+            Assert.Equal(2046, Day04CeresSearch.FindMasX(PuzzleData.DayFour));
+        }
+    }
+}
