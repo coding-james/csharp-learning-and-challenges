b2df83ea4639f3ba6ecd3be8e74387650c41a640
James <98489957+coding-james@users.noreply.github.com>
2024-12-07 23:02:07 +0000

    Day 5 complete

---
+++ src/Sandbox/AdventOfCode2024/Day05PrintQueue.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.AdventOfCode2024
+{
+    public class Day05PrintQueue
+    {
+
+        public static int GiveMiddleValue(string input, bool giveCorrectedOnly = false)
+        {
+            int result = 0;
+            List<List<int>> ordersRules = new List<List<int>>();
+            List<List<int>> printOrder = new List<List<int>>();
+            List<List<int>> correctPrintOrder = new List<List<int>>();
+            List<List<int>> updatedPrintOrder = new List<List<int>>();
+            List<List<List<int>>> cleansed = Cleanse(input);
+            List<bool> results = new List<bool>();
+            bool isUpdatedInValid = true;
+
+            ordersRules = cleansed[0];
+            printOrder = cleansed[1];
+
+            for (int printIndex = 0; printIndex < printOrder.Count; printIndex++)
+            {
+                bool isOriginalValid = true;
+
+                for (int ruleIndex = 0; ruleIndex < ordersRules.Count; ruleIndex++)
+                {
+                    {
+                        int ruleOne = ordersRules[ruleIndex][0];
+                        int ruleTwo = ordersRules[ruleIndex][1];
+
+                        if (printOrder[printIndex].Contains(ruleOne) && printOrder[printIndex].Contains(ruleTwo))
+                        {
+                            int indexOfFirst = printOrder[printIndex].IndexOf(ruleOne);
+                            int indexOfSecond = printOrder[printIndex].IndexOf(ruleTwo);
+
+                            if (indexOfFirst > indexOfSecond)
+                            {
+                                isOriginalValid = false;
+                                if (giveCorrectedOnly)
+                                {
+                                    printOrder[printIndex][indexOfFirst] = ruleTwo;
+                                    printOrder[printIndex][indexOfSecond] = ruleOne;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (isOriginalValid)
+                {
+                    correctPrintOrder.Add(printOrder[printIndex]);
+                }
+                else
+                {
+                    updatedPrintOrder.Add(printOrder[printIndex]);
+                }
+            }
+
+            if (giveCorrectedOnly)
+            {
+                while (isUpdatedInValid)
+                {
+                    results.Clear();
+                    for (int printIndex = 0; printIndex < updatedPrintOrder.Count; printIndex++)
+                    {
+                        for (int ruleIndex = 0; ruleIndex < ordersRules.Count; ruleIndex++)
+                        {
+                            {
+                                int ruleOne = ordersRules[ruleIndex][0];
+                                int ruleTwo = ordersRules[ruleIndex][1];
+
+                                if (updatedPrintOrder[printIndex].Contains(ruleOne) && updatedPrintOrder[printIndex].Contains(ruleTwo))
+                                {
+                                    int indexOfFirst = updatedPrintOrder[printIndex].IndexOf(ruleOne);
+                                    int indexOfSecond = updatedPrintOrder[printIndex].IndexOf(ruleTwo);
+
+                                    if (indexOfFirst > indexOfSecond)
+                                    {
+                                        results.Add(false);
+                                        updatedPrintOrder[printIndex][indexOfFirst] = ruleTwo;
+                                        updatedPrintOrder[printIndex][indexOfSecond] = ruleOne;
+                                    }
+                                    else
+                                    {
+                                        results.Add(true);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (results.Contains(false))
+                    {
+                        isUpdatedInValid = true;
+                    }
+                    else
+                    {
+                        isUpdatedInValid = false;
+                    }
+                }
+                return CalcMiddleValue(updatedPrintOrder);
+            }
+
+            return CalcMiddleValue(correctPrintOrder);
+        }
+
+        //Cleanse the input
+        public static List<List<List<int>>> Cleanse(string input)
+        {
+            List<List<List<int>>> cleansed = new List<List<List<int>>>();
+            List<List<int>> cleansedRules = new List<List<int>>();
+            List<List<int>> cleansedPages = new List<List<int>>();
+
+            List<string> firstPass = input.Split(Environment.NewLine).ToList();
+
+            for (int index = 0; index < firstPass.Count; index++)
+            {
+                if (firstPass[index].Contains("|"))
+                {
+                    List<int> tempRules = new List<int>();
+                    string tempStr = firstPass[index];
+                    int dividerIndex = tempStr.IndexOf("|");
+                    tempRules.Add(int.Parse(tempStr.Substring(0, dividerIndex)));
+                    tempRules.Add(int.Parse(tempStr.Substring(dividerIndex + 1)));
+                    cleansedRules.Add(tempRules);
+                }
+                else if (firstPass[index] != "")
+                {
+                    List<int> tempOrderNum = new List<int>();
+                    List<string> tempOrder = firstPass[index].Split(",").ToList();
+                    foreach (var item in tempOrder)
+                    {
+                        tempOrderNum.Add(int.Parse(item));
+                    }
+                    cleansedPages.Add(tempOrderNum);
+                }
+            }
+            cleansed.Add(cleansedRules);
+            cleansed.Add(cleansedPages);
+
+            return cleansed;
+        }
+
+        public static int CalcMiddleValue(List<List<int>> printOrder)
+        {
+            int result = 0;
+
+            for (int printIndex = 0; printIndex < printOrder.Count; printIndex++)
+            {
+                int middle = printOrder[printIndex].Count / 2;
+                result += printOrder[printIndex][middle];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/Sandbox.Tests/AdventOfCode2024Tests/Day05PrintQueueTest.cs
@@ -0,0 +1,46 @@
+﻿using Sandbox.AdventOfCode2024;
+using Sandbox.Tests.AdventOfCode2024Tests.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Tests.AdventOfCode2024Tests
+{
+    public class Day05PrintQueueTest
+    {
+        string exampleOne = "47|53\r\n97|13\r\n97|61\r\n97|47\r\n75|29\r\n61|13\r\n75|53\r\n29|13\r\n97|29\r\n53|29\r\n61|53\r\n97|53\r\n61|29\r\n47|13\r\n75|47\r\n97|75\r\n47|61\r\n75|61\r\n47|29\r\n75|13\r\n53|13\r\n\r\n75,47,61,53,29\r\n97,61,53,29,13\r\n75,29,13\r\n75,97,47,61,53\r\n61,13,29\r\n97,13,75,29,47";
+
+        [Fact]
+        public void Test_Cleanse()
+        {
+            List<List<List<int>>> expected = [[[47, 53], [97, 13], [97, 61], [97, 47], [75, 29], [61, 13], [75, 53], [29, 13], [97, 29], [53, 29], [61, 53], [97, 53], [61, 29], [47, 13], [75, 47], [97, 75], [47, 61], [75, 61], [47, 29], [75, 13], [53, 13]], [[75, 47, 61, 53, 29], [97, 61, 53, 29, 13], [75, 29, 13], [75, 97, 47, 61, 53], [61, 13, 29], [97, 13, 75, 29, 47]]];
+            Assert.Equal(expected, Day05PrintQueue.Cleanse(exampleOne));
+        }
+
+        [Fact]
+        public void Test_Example()
+        {
+            Assert.Equal(143, Day05PrintQueue.GiveMiddleValue(exampleOne));
+        }
+
+        [Fact]
+        public void Test_Puzzle_Part_One()
+        {
+            Assert.Equal(7307, Day05PrintQueue.GiveMiddleValue(PuzzleData.DayFive));
+        }
+
+        [Fact]
+        public void Test_Example_Part_Two()
+        {
+            Assert.Equal(123, Day05PrintQueue.GiveMiddleValue(exampleOne, true));
+        }
+
+        [Fact]
+        public void Test_Puzzle_Part_Two()
+        {
+            Assert.Equal(4713, Day05PrintQueue.GiveMiddleValue(PuzzleData.DayFive, true));
+        }
+    }
+}
